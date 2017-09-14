4b2bbb434f491195cda36224535ddc2ef8b58204
scollins33 <sean_collins@gmx.com>
2017-09-13 20:30:45 -0400

    added space replacement in button creation function

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -2,7 +2,9 @@
 $('#search-button').on('click', function () {
     // get value of input box
     var searchValue = $('#search-input').val();
+    var queryValue = searchValue.replace(' ', '%20');
 
     // create button and assign value
-    $('#terms-div').append('<button class="btn btn-default term-tag" value="' + searchValue + '">' + searchValue + '</button>');
+    $('#terms-div').append('<button class="btn btn-default term-tag" value="' + queryValue + '">' + searchValue + '</button>');
 });
+
